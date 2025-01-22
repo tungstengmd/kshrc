836baf04102b5a4738ae6761a39347b94a3c7c2d
Harlow <harley@the-machine.lan>
2025-01-22 20:14:26 +0000

    good prompt

--- kshScripts/prompt.sh
+++ kshScripts/prompt.sh
@@ -3,4 +3,5 @@ bgblack="[40m"
 green="[92m"
 black="[30m"
 white="[0m"
-export PS1='${green}${black}${bggreen}${PWD##*/}${bgblack}${green}─> ${white}'
+export PS1=$(printf '${green}╭─{owo} ${USER} in ${black}${bggreen}${PWD##*/}${bgblack}${green}
+${green}╰──> ${white}')
