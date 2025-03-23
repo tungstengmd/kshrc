3c84dc8e107f48af0a682a4da7224662465fd02f
harlow foxworthy <118553985+tungstengmd@users.noreply.github.com>
2025-03-23 16:56:36 +0000

    Update prompt.sh

--- kshScripts/prompt.sh
+++ kshScripts/prompt.sh
@@ -1,16 +1,19 @@
-PS1='$(exitcolor=$([ $? = 0 ] && echo "\033[92m╰──" || echo "\033[91mx  ");
-gitstat="$(LC_ALL=C GIT_OPTIONAL_LOCKS=0 env git status 2>&1)";
+PS1='$(exitcolor=$([ $? = 0 ] && echo "\033[92m╰──" || echo "\033[91mx  ")
+gitstat="$(git status 2>&1)"
 symb=''
 case $gitstat in
-    *"has diverged"*) symb+='%' ;;
-    *"branch is behind"*) symb+="<" ;;
-    *"ahead of"*) symb+=">" ;;
-    *"new file:"*) symb+='+' ;;
-    *"deleted:"*) symb+='!!' ;;
-    *"renamed"*) symb+='R' ;;
-    *"Untracked"*) symb+='U' ;;
-    *) symb+=":3"
+    *"has diverged"*) symb+='%' ;&
+    *"branch is behind"*) symb+="<" ;& 
+    *"ahead of"*) symb+=">" ;&
+    *"new file:"*) symb+='A' ;&
+    *"deleted:"*) symb+='D' ;&
+    *"renamed"*) symb+='R' ;&
+    *"Untracked"*) symb+='U' ;&
 esac
-printf "\033[92m╭─{owo}─{"$(date +%H):$(date +%M)"} ${USER} in \033[30m\033[102m$(pwd | sed "s|$HOME|~|")\033[49m\033[92m $([ "$(git status >/dev/null 2>&1; echo $?)" = 0 ] && echo "($(git branch --show-current)) [$symb]")\n${exitcolor}> \033[0m";
-
+printf "\033[92m╭─{owo}─\
+{"$(date +%H):$(date +%M)"}\
+$([ $USER = root ] && echo "\033[91m" || echo "\033[93m") ${USER} \
+\033[92min \033[30m\033[102m$(pwd | sed "s|$HOME|~|")\033[49m\033[92m\
+$([ "$(git status >/dev/null 2>&1; echo $?)" = 0 ] && echo "($(git branch --show-current))") $([ "$symb" = "" ] || echo "[$symb]")\
+\n${exitcolor}> \033[0m" 
 )'
